f808b1bab5f9aca9546f5cfeb57a6ae4fe1c6a53
GhabrielAngello <ghabrielangelo13@gmail.com>
2021-11-12 12:30:13 -0300

    Conceitos

---
+++ web-react-conceitos/src/Macgyver.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Macgyver from './Macgyver';
+ 
+class App extends Component {
+ 
+  render(){
+    return(
+      <div>
+        <Macgyver />
+      </div>
+    );
+  }
+}
+ 
+export default App;
